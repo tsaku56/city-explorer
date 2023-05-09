187ffa1f49fd636f606f434501454940067c1985
Ross <blindross@gmail.com>
2023-05-09 15:13:30 +0100

    big crimes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [location, setLocation] = useState({});
+  const [mapIMG, setMap] = useState("");
 
   function handleChange(event) {
     setSearchQuery(event.target.value);
@@ -15,6 +16,8 @@ function App() {
       const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
       const res = await axios.get(API);
       setLocation(res.data[0]);
+      const showMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_TOKEN}&center=${location.lat},${location.lon}$zoom=12&size=600x400&format=png`;
+      setMap(showMap);
     } catch (error) {
       console.log(error);
     }
@@ -28,6 +31,7 @@ function App() {
       <h2>{location.display_name}</h2>
       <h2>Latitude={location.lat}</h2>
       <h2>Longitude={location.lon}</h2>
+      {/* <img src=`${mapIMG.show}` /> */}
     </div>
   );
 }
